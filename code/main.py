ae5b77eaf366422457be5d01ce647a996760d0a3
ali <aallii.r98@gmail.com>
2022-01-23 19:56:07 +0330

    menu functions

--- code/main.py
+++ code/main.py
@@ -65,6 +65,98 @@ class Graph:
                 visited[self.graph.get(x)[i]] = 1
         return distance[v]
 
+def setPriority(user1, user2, speciality, field, university, workplace):
+    i = 0
+    for x in user2.specialities:
+        if x in user1.specialities:
+            i += 1
+    user2.priority += (speciality * i)
+    if user1.field == user2.field:
+        user2.priority += field
+    if user1.universityLocation == user2.universityLocation:
+        user2.priority += university
+    if user1.workplace == user2.workplace:
+        user2.priority += workplace
+    user2.priority -= user2.connectionDegree
+
+
+def show(user1, p):
+    print(f"{p + 1}) priority: [{user1.priority}] Id: [{user1.id}] Name: [{user1.name}] Date of birth: "
+          f"[{user1.dateOfBirth}] Field: [{user1.field}] University: [{user1.universityLocation}] Work place: "
+          f"[{user1.workplace}] Specialities: {user1.specialities} Connection Ids: {user1.connectionId}")
+
+
+def find_connection(users, id):
+    if id in users:
+        user1 = users[id]
+        connections = g.BFS(int(id))
+        list = []
+        print("""
+Enter a number between 0 and 10
+                            """)
+        speciality = int(input("speciality:"))
+        field = int(input("field:"))
+        university = int(input("university:"))
+        workplace = int(input("workplace:"))
+        for p in connections:
+            s = users[str(p)]
+            s.connectionDegree = connections[p]
+            s.priority = 0
+            setPriority(user1, s, speciality, field, university, workplace)
+            list.append((s.priority, s))
+        list.sort(reverse=True)
+        for p in range(len(list)):
+            if p < 20:
+                show(list[p][1], p)
+    else:
+        print("there isn't any account with this id in linkedin")
+
+
+def register_findConnection(user, users):
+    list = []
+    i=0
+    for p in users:
+        if i <20:
+            s = users[p]
+            s.priority = 0
+            s.connectionDegree = 0
+            setPriority(user, s, 10, 7, 5, 3)
+            list.append((s.priority, s))
+    list.sort(reverse=True)
+    for p in range(len(list)):
+        if p < 20:
+            show(list[p][1], p)
+
+
+def register(users, g):
+    id = input("Id: ")
+    if id not in users:
+        name = input("name: ")
+        date = input("date of birth: ")
+        university = input("university: ")
+        field = input("field: ")
+        workplace = input("workplace: ")
+        speciality = []
+        connections1 = []
+        for x in range(int(input("how many speciality: "))):
+            speciality.append(input(str(x + 1) + ") "))
+        print("which person do you want to connect?")
+        user2 = user(id, name, date, university, field, workplace, speciality, connections1)
+        register_findConnection(user2, users)
+        print("how many?")
+        for i in range(int(input())):
+            connections1.append(input("Enter id: "))
+
+        user2.connectionId = connections1
+
+        for i in user2.connectionId:
+                g.graph[int(i)].append(int(user2.id))
+        g.addEdge(int(user2.id), user2.connectionId)
+        users[user2.id] = user2
+        print("account created")
+    else:
+        print("there is another account with this id")
+
 if __name__ == '__main__':
 
     with open("users.json", 'r') as file:
@@ -76,3 +168,19 @@ if __name__ == '__main__':
     g = Graph()
     for p in users:
         g.addEdge(int(users[p].id), users[p].connectionId)
+
+    print("welcome to my linkedin")
+    user1 = None
+    while True:
+        print("""      
+what do you want to do?
+1) see the list of connections
+2) register an connect
+            """)
+        do = input()
+        if do == "1":
+            id = input("Enter id: ")
+            find_connection(users, id)
+
+        elif do == "2":
+            register(users, g)
